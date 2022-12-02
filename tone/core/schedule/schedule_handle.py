162de44b1f8a5f9c0622d30f5102468efa10b6f3
wb-wk01394711 <wb-wk01394711@alibaba-inc.com>
2022-12-02 18:06:56 +0800

    fix: 测试计划定时任务未按照实际配置时间运行

--- tone/core/schedule/schedule_handle.py
+++ tone/core/schedule/schedule_handle.py
@@ -20,12 +20,24 @@ class ScheduleHandle(ScheduleJob):
         https://apscheduler.readthedocs.io/en/latest/modules/triggers/cron.html#module-apscheduler.triggers.cron
         """
         minute, hour, day, month, day_of_week = expression.split(' ')
-        if day_of_week.isdigit():
-            day_of_week = (int(day_of_week) + 6) % 7
+        real_day_of_week = cls.day_of_week_format(day_of_week)
         job = scheduler.add_job(job_function, trigger='cron', args=args, minute=minute, hour=hour,
-                                day=day, month=month, day_of_week=day_of_week,
+                                day=day, month=month, day_of_week=real_day_of_week,
                                 misfire_grace_time=600)
         ScheduleMap.objects.create(schedule_job_id=job.id, object_id=args[0])
+
+    @classmethod
+    def day_of_week_format(cls, day_of_week):
+        # 此方法为了解决添加定时任务时格式效验和实际运行定时任务两个模块对day_of_week差异
+        # 添加定时任务效验定时任务格式使用CronTrigger.from_crontab(cron_express)，该模块中1-6代表周一到周六，0代表周天
+        # 实际使用scheduler.add_job添加定时任务时，day_of_week参数中，0代表周一，1代表周二，2代表周三，***，5代表周六，6代表周天
+        real_day_of_week = ""
+        for i in day_of_week:
+            if not i.isdigit():
+                real_day_of_week += i
+            else:
+                real_day_of_week += str((int(i) + 6) % 7)
+        return real_day_of_week
 
     @classmethod
     def add_standard_job(cls, job_function, trigger_type, **kwargs):
